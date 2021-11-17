87fc0aa105235a24d08a81fd4e47207d9ec0ff13
Nguyen Quoc Khanh <nguyenkhanhcv@gmail.com>
2021-11-17 15:44:25 +0700

    Add Swagger, Order and Payment API

--- pstore/admin.py
+++ pstore/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from pstore.models import Product, Customer, ItemCart, Order, Review
+from pstore.models import Product, ItemCart, Order, Review
 
 admin.site.register(Product)
-admin.site.register(Customer)
 admin.site.register(ItemCart)
 admin.site.register(Order)
 admin.site.register(Review)
--- pstore/urls.py
+++ pstore/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.conf.urls import url
 
-from pstore.views import ListCreateProductView, ListReviewByProduct, CartView
+from pstore.views import ListCreateProductView, ListReviewByProduct, CartView, OrderView, PaymentAPIView
 
 urlpatterns = [
     url(r'^products', ListCreateProductView.as_view()),
     url(r'^products/<int:pk>/reviews$', ListReviewByProduct.as_view()),
     url(r'^cart', CartView.as_view()),
+    url(r'^order', OrderView.as_view()),
+    url(r'^payment', PaymentAPIView.as_view()),
 ]
